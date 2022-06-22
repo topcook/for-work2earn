685ec5dd0cd23ce672f60817fb4fa0f4f04b2668
topcook <codecooker1996@gmail.com>
2022-06-22 18:43:06 -0400

    switch network

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -76,8 +76,8 @@ export class Dapp extends React.Component {
     // clicks a button. This callback just calls the _connectWallet method.
     if (!this.state.selectedAddress) {
       return (
-        <ConnectWallet 
-          connectWallet={() => this._connectWallet()} 
+        <ConnectWallet
+          connectWallet={() => this._connectWallet()}
           networkError={this.state.networkError}
           dismiss={() => this._dismissNetworkError()}
         />
@@ -176,13 +176,59 @@ export class Dapp extends React.Component {
     this._stopPollingData();
   }
 
+  async _switchNetwork() {
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x7A69" }],
+      });
+    } catch (e) {
+      console.log(e);
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x7A69",
+            chainName: 'localhost',
+            rpcUrls: 'http://127.0.0.1:8545',
+            nativeCurrency: 'TTFW',
+            blockExplorerUrls: '',
+          },
+        ],
+      });
+    }
+  };
+
+
   async _connectWallet() {
     // This method is run when the user clicks the Connect. It connects the
     // dapp to the user's wallet, and initializes it.
 
     // To connect to the user's wallet, we have to run this method.
     // It returns a promise that will resolve to the user's address.
-    const [selectedAddress] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+    let chainId = await window.ethereum.request({ method: "eth_chainId" });
+    console.log("chainId: ", chainId);
+    if (!(chainId === "0x7a69")) {
+      await this._switchNetwork();
+      await delay(2000);
+    }
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    const reloadApp = () => {
+      window.location.reload();
+    };
+
+    window.ethereum.on("chainChanged", reloadApp);
+    window.ethereum.on("accountsChanged", reloadApp);
+    window.ethereum.on("disconnect", reloadApp);
+
+    const selectedAddress = accounts[0];
 
     // Once we have the address, we can initialize the application.
 
@@ -203,10 +249,10 @@ export class Dapp extends React.Component {
       if (newAddress === undefined) {
         return this._resetState();
       }
-      
+
       this._initialize(newAddress);
     });
-    
+
     // We reset the dapp state if the network is changed
     window.ethereum.on("chainChanged", ([networkId]) => {
       this._stopPollingData();
@@ -375,7 +421,7 @@ export class Dapp extends React.Component {
       return true;
     }
 
-    this.setState({ 
+    this.setState({
       networkError: 'Please connect Metamask to Localhost:8545'
     });
 
